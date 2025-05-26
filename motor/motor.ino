7b2c330c20bcf6260d38418d8cc87ac5727ccf56
JESSED30 <1332023042@alumnos.unp.edu.pe>
2025-05-26 09:29:32 -0500

    Cambio desde Bloc de notas: velocidad ajustada

--- motor/motor.ino
+++ motor/motor.ino
@@ -2,7 +2,7 @@ const int pinIN1 = 4;   // Sentido horario
 const int pinIN2 = 5;   // Sentido antihorario
 const int pinENA = 6;   // PWM
 
-int velocidad = 50;    // Velocidad por defecto
+int velocidad = 120;    // Velocidad por defecto
 
 // --- FUNCIONES PARA CONTROLAR MOTOR ---
 
